e478dc77ad2d58e8cf423d68b4451e08fc0b3117
Triloki Nath <trilokinath35@gmail.com>
2022-05-03 11:03:32 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,51 @@
+const button1 = document.getElementById("btn1");
+
+button1.addEventListener("click" , function(){
+    var audio = new Audio("sounds/tom-1.mp3")
+    audio.play();
+})
+
+
+const buttom2 = document.getElementById("btn2");
+
+buttom2.addEventListener("click" , function(){
+    var audio = new Audio("sounds/tom-2.mp3")
+    audio.play();
+})
+
+const buttom3 = document.getElementById("btn3");
+
+buttom3.addEventListener("click" , function(){
+    var audio = new Audio("sounds/tom-3.mp3")
+    audio.play();
+})
+
+const buttom4 = document.getElementById("btn4");
+
+buttom4.addEventListener("click" , function(){
+    var audio = new Audio("sounds/tom-4.mp3")
+    audio.play();
+})
+
+const buttom5 = document.getElementById("btn5");
+
+buttom5.addEventListener("click" , function(){
+    var audio = new Audio("sounds/snare.mp3")
+    audio.play();
+})
+
+
+const buttom6 = document.getElementById("btn6")
+
+buttom6.addEventListener("click" , function(){
+    var audio = new Audio("sounds/kick-bass.mp3")
+    audio.play();
+})
+
+
+const buttom7 = document.getElementById("btn7")
+
+buttom7.addEventListener("click" , function(){
+    var audio = new Audio("sounds/crash.mp3")
+    audio.play();
+})
